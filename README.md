90fc924178649f6eedc57740fcd71f905737788b
nevinedwin <nevinedwin100@gmail.com>
2023-08-23 14:16:56 +0530

    home page completed

--- README.md
+++ README.md
@@ -37,3 +37,5 @@ Check out our [Next.js deployment documentation](https://nextjs.org/docs/deploym
 ########################################################################################################################################################
 
 icons downloaded from : https://icon-sets.iconify.design
+
+convert svg to react component : https://react-svgr.com/playground/
---
+++ src/components/static.js
@@ -0,0 +1,24 @@
+import { GithubIcon, LeetCodeIcon, LinkedInIcon, TwitterIcon } from "./Icons";
+
+export const IconData = [
+    {
+        href: "https://twitter.com",
+        component: <TwitterIcon/>
+    },
+    {
+        href: "https://github.com",
+        component: <GithubIcon/>
+    },
+    {
+        href: "https://linkedin.com",
+        component: <LinkedInIcon/>
+    },
+    {
+        href: "https://leetcode.com",
+        component: <LeetCodeIcon/>
+    }
+] 
+
+export const homePageHeading = "Turning Vision Into Reality With Code And Design"
+
+export const homePageContent = "As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications.Explore my latest projects and articles, showcasing my expertise in React.js and web development"
