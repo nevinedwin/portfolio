4f5f5b69f51ff3c09a99caabd6cec31a6d4e1ff8
nevinedwin <nevinedwin100@gmail.com>
2023-08-27 23:04:13 +0530

    test

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from './Logo';
 import { useRouter } from 'next/router';
-import {motion} from 'framer-motion';
+import { motion } from 'framer-motion';
 import { IconData } from './static';
 import useThemeSwitcher from './hooks/useThemeSwitcher';
 import { MoonIcon, SunIcon } from './Icons';
 
-const CustomLink = ({href, title, className=""})=>{
+const CustomLink = ({ href, title, className = "" }) => {
   const router = useRouter();
   return (
     <Link href={href} className={`${className} relative group`}>
@@ -16,7 +16,7 @@ const CustomLink = ({href, title, className=""})=>{
         h-[1px] inline-block bg-dark 
         absolute left-0 -bottom-0.5 
         group-hover:w-full transition-[width] ease duration-300
-        ${router.asPath === href ?  'w-full' : 'w-0'} dark:bg-light
+        ${router.asPath === href ? 'w-full' : 'w-0'} dark:bg-light
       `}>
         &nbsp;
       </span>
@@ -24,39 +24,115 @@ const CustomLink = ({href, title, className=""})=>{
   )
 }
 
+
+const CustomMobileLink = ({ href, title, className = "", toggle }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    toggle();
+    router.push(href)
+  }
+
+  return (
+    <button className={`${className} relative group text-light dark:text-dark my-2`} onClick={handleClick}>
+      {title}
+      <span className={`
+        h-[1px] inline-block bg-light 
+        absolute left-0 -bottom-0.5 
+        group-hover:w-full transition-[width] ease duration-300
+        ${router.asPath === href ? 'w-full' : 'w-0'} dark:bg-dark
+      `}>
+        &nbsp;
+      </span>
+    </button>
+  )
+};
+
 const Navbar = () => {
   const [mode, setMode] = useThemeSwitcher();
-  return (
-    <header className='w-full px-32 py-8 font-medium flex item-center justify-between dark:text-light'>
-      <nav>
-        <CustomLink href='/' title='Home' className='mr-4'/>
-        <CustomLink href='/about' title='About'className='mx-4'/>
-        <CustomLink href='/projects' title='Projects'className='mx-4'/>
-        <CustomLink href='/articles' title='Articles' className='ml-4'/>
-      </nav>
-      <nav className='flex items-center justify-center flex-wrap'>
-        {
-          IconData.map((eachItem, index)=>(
-            <motion.a key={index} href={eachItem.href} target={'_blank'}
-              whileHover={{y:-2}}
-              whileTap={{scale: 0.9}}
-              className='w-6 mx-3'
-            >
-              {eachItem.component}
-            </motion.a>
-          ))
-        }      
+  const [isOpen, setIsOpen] = useState(false);
 
-        <button onClick={()=>setMode(mode==="light"? "dark": "light")} className={`ml-3 flex items-center justify-center rounded-full p-1 ${mode==="light" ? 'bg-dark text-light' : 'bg-light text-dark'}`}>
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <header className='w-full px-32 py-8 font-medium flex item-center justify-between relative dark:text-light'>
+      <button className='flex-col justify-center items-center hidden lg:flex' onClick={handleClick}>
+        <span className={`bg-dark dark:bg-light transition-all duration-300 ease-out block h-0.5 w-6 rounded-sm  ${isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'}`}></span>
+        <span className={`bg-dark dark:bg-light transition-all duration-300 ease-out block h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? 'opacity-0' : 'opacity-100'}`}></span>
+        <span className={`bg-dark dark:bg-light transition-all duration-300 ease-out block h-0.5 w-6 rounded-sm ${isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'}`}></span>
+      </button>
+
+      {/* desktop Nav */}
+      <div className='w-full flex items-center justify-between lg:hidden'>
+        <nav>
+          <CustomLink href='/' title='Home' className='' />
+          <CustomLink href='/about' title='About' className='' />
+          <CustomLink href='/projects' title='Projects' className='' />
+          <CustomLink href='/articles' title='Articles' className='' />
+        </nav>
+        <nav className='flex items-center justify-center flex-wrap'>
           {
-            mode === "dark"? <SunIcon className={"fill-dark"}/> : <MoonIcon className={"fill-dark"}/>
+            IconData.map((eachItem, index) => (
+              <motion.a key={index} href={eachItem.href} target={'_blank'}
+                whileHover={{ y: -2 }}
+                whileTap={{ scale: 0.9 }}
+                className='w-6 mx-3'
+              >
+                {eachItem.component}
+              </motion.a>
+            ))
           }
-        </button>  
-      </nav>
-      <div className='absolute left-[50%] top-2 translate-x-[50%]'>
-        <Logo/>
+
+          <button onClick={() => setMode(mode === "light" ? "dark" : "light")} className={`ml-3 flex items-center justify-center rounded-full p-1 ${mode === "light" ? 'bg-dark text-light' : 'bg-light text-dark'}`}>
+            {
+              mode === "dark" ? <SunIcon className={"fill-dark"} /> : <MoonIcon className={"fill-dark"} />
+            }
+          </button>
+        </nav>
       </div>
-    </header>
+
+      {
+        isOpen ?
+          <>
+            {/* Mobile Nav */}
+            < div className='min-w-[70vw] flex flex-col items-center justify-between fixed top-1/2 left-1/2 -translate-x-1/2
+      -translate-y-1/2 z-30 bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32'>
+              <nav className='flex items-center flex-col justify-center'>
+                <CustomMobileLink href='/' title='Home' className='mr-4' toggle={handleClick} />
+                <CustomMobileLink href='/about' title='About' className='mx-4' toggle={handleClick} />
+                <CustomMobileLink href='/projects' title='Projects' className='mx-4' toggle={handleClick} />
+                <CustomMobileLink href='/articles' title='Articles' className='ml-4' toggle={handleClick} />
+              </nav>
+              <nav className='flex items-center justify-center flex-wrap'>
+                {
+                  IconData.map((eachItem, index) => (
+                    <motion.a key={index} href={eachItem.href} target={'_blank'}
+                      whileHover={{ y: -2 }}
+                      whileTap={{ scale: 0.9 }}
+                      className={`w-6 mx-3 `}
+                    >
+                      {eachItem.component}
+                    </motion.a>
+                  ))
+                }
+
+                <button onClick={() => setMode(mode === "light" ? "dark" : "light")} className={`ml-3 flex items-center justify-center rounded-full p-1 ${mode === "light" ? 'bg-dark text-light' : 'bg-light text-dark'}`}>
+                  {
+                    mode === "dark" ? <SunIcon className={"fill-dark"} /> : <MoonIcon className={"fill-dark"} />
+                  }
+                </button>
+              </nav>
+            </div>
+          </>
+          : null
+      }
+
+      <div className='absolute left-[50%] top-2 translate-x-[50%]'>
+        <Logo />
+      </div>
+    </header >
   )
 }
 
