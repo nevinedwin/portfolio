703ecee769fae9f181c22ab04563b0428906367a
nevinedwin <nevinedwin100@gmail.com>
2023-08-29 00:23:40 +0530

    text

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -5,7 +5,7 @@ import Link from 'next/link'
 const HireMe = () => {
   return (
     <div className='fixed left-4 bottom-4 flex items-center justify-center overflow-hidden 
-      md:right-8 md:left-auto md:top-0 md:bottom-auto md:absolute z-40
+      md:right-4 md:left-auto md:top-0 md:bottom-auto md:absolute z-40
     '>
       <div className='w-48 h-auto flex justify-center items-center relative md:w-24'  >
         <CircularText className='fill-dark animate-spin-slow dark:!fill-light ' />
