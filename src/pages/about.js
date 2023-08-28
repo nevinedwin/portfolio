4bbdd250b3b14e90f7113fe19952448f6f1f867a
nevinedwin <nevinedwin100@gmail.com>
2023-08-29 00:44:07 +0530

    test

--- src/pages/about.js
+++ src/pages/about.js
@@ -44,9 +44,11 @@ const about = () => {
       </Head>
       <main className='flex w-full flex-col items-center justify-center dark:text-light'>
         <Layout className='pt-16'>
-          <AnimatedText text={aboutHeading} className='mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl' />
-          <div className='grid w-full grid-cols-8 gap-16'>
-            <div className='col-span-3 flex flex-col items-start justify-start'>
+          <AnimatedText text={aboutHeading} className='mb-16 lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8' />
+          <div className='grid w-full grid-cols-8 gap-16 sm:gap-8'>
+            <div className='col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2
+            md:col-span-8
+            '>
               <h2 className='mb-4 text-lg font-bold uppercase text-dark dark:text-light opacity-75'>
                 About Me
               </h2>
@@ -58,7 +60,9 @@ const about = () => {
             </div>
             <div
               className='col-span-3 relative h-max rounded-2xl border-2 border-solid 
-            border-dark dark:border-light bg-light dark:bg-dark p-8'
+            border-dark dark:border-light bg-light dark:bg-dark p-8 xl:col-span-4
+            md:order-1 md:col-span-8
+            '
             >
               <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark dark:bg-white' />
               <Image
@@ -71,16 +75,20 @@ const about = () => {
                 33vw'
               />
             </div>
-            <div className='col-span-2 flex flex-col items-end justify-between'>
+            <div className='col-span-2 flex flex-col items-end justify-between xl:col-span-8 xl:flex-row xl:items-center 
+            md:order-3
+            '>
               {aboutStatus.map((eachStatus, index) => (
                 <div
                   key={index}
-                  className='flex flex-col items-end justify-center'
+                  className='flex flex-col items-end justify-center xl:items-center'
                 >
-                  <span className='inline-block text-7xl font-bold'>
+                  <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
                     <AnimatedNumbers value={eachStatus.number} />+
                   </span>
-                  <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75'>
+                  <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75
+                  xl:text-center md:text-lg sm:text-base xs:text-sm
+                  '>
                     {eachStatus.content}
                   </h2>
                 </div>
